7e555a761b1813a78e8187b435ffaed6b158598b
mika <2865911620@qq.com>
2024-05-14 17:34:52 +0800

    feat: Rust的循环语句

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use std::io;
 fn main() {
     println!("猜猜数字!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("请输入你的猜测");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("读取失败");
-    let guess: u32 = guess.trim().parse().expect("输入值转换无效");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("太小了"),
-        Ordering::Greater => println!("太大了"),
-        Ordering::Equal => println!("恭喜你，猜对了！"),
+    loop {
+        println!("请输入你的猜测");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取失败");
+        let guess: u32 = guess.trim().parse().expect("输入值转换无效");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("恭喜你，猜对了！");
+                break;
+            }
+        }
     }
 }
