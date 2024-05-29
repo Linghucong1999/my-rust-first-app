ee23a58549bc6fc2a7d473a84451c165ae27686e
mika <2865911620@qq.com>
2024-05-29 11:37:20 +0800

    feat: 函数开发的应用

--- src/variable-not.rs
+++ src/variable-not.rs
@@ -1,10 +1,32 @@
-use std::io;
+// use std::io;
 fn main() {
-    let a = [3, 1, 2, 4, 5, 7, 6];
-    println!("输入访问的元素索引：");
-    let mut index = String::new();
-    io::stdin().read_line(&mut index).expect("读取失败");
-    let index: usize = index.trim().parse().expect("输入错误");
-    let element = a[index];
-    println!("访问的元素的索引是{}，元素是{}",index, element);
+    // 数组类型
+    // let a = [3, 1, 2, 4, 5, 7, 6];
+    // println!("输入访问的元素索引：");
+    // let mut index = String::new();
+    // io::stdin().read_line(&mut index).expect("读取失败");
+    // let index: usize = index.trim().parse().expect("输入错误");
+    // let element = a[index];
+    // println!("访问的元素的索引是{}，元素是{}",index, element);
+
+    // 函数开发
+
+    anothor_function(5, 6);
+}
+
+fn anothor_function(x: i64, y: i64) {
+    let z = {
+        let a = x + y;
+        let b = a * 2;
+        let c = five();
+        five_plus(b + c)
+    };
+    println!("z is {}", z);
+}
+
+fn five() -> i64 {
+    200
+}
+fn five_plus(x: i64) -> i64 {
+    x + 5
 }
